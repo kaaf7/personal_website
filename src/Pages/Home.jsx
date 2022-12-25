9fab1275947ec634183a46051d991537faa3f621
kaaf7 <amer.m.karim@gmail.com>
2022-12-25 14:30:59 +0100

    Ad all page components to Home page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Components/Navbar";
+import UpperSection from "../Components/UpperSection";
+import MidSection from "../Components/MidSection";
+import LowerSection from "../Components/LowerSection";
+import Footer from "../Components/Footer";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <Navbar />
+      <UpperSection />
+      <MidSection />
+      <LowerSection />
+      <Footer />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
