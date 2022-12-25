69eb0bbea45392af7059f1e5456f3541c5d7e514
kaaf7 <amer.m.karim@gmail.com>
2022-12-25 14:30:06 +0100

    Create Upper Section Silhouette

---
+++ src/Components/UpperSection.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: #F9F9F9;
+`;
+
+const UpperSection = () => {
+  return <Container></Container>;
+};
+
+export default UpperSection;
