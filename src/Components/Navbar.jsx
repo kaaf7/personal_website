1e2042bb68801d4934cee5f6322b358786cf9314
kaaf7 <amer.m.karim@gmail.com>
2022-12-25 14:30:41 +0100

    Create Navbar Silhouette

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 10vh;
+  background-color: #f3e1fa;
+  position: fixed;
+`;
+const Navbar = () => {
+  return <Container></Container>;
+};
+
+export default Navbar;
