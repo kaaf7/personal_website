e1a020cd0c1360c887ca8bf547e1e0ef5fc02a8b
kaaf7 <amer.m.karim@gmail.com>
2022-12-25 14:30:31 +0100

    Create Mid Section Silhouette

---
+++ src/Components/MidSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 80vh;
+  background-color: #CDF0EA;
+`;
+const MidSection = () => {
+  return <Container></Container>;
+};
+
+export default MidSection;
