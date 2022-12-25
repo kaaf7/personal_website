296addd037ff655f7fb19cf8f477e3c9b684653b
kaaf7 <amer.m.karim@gmail.com>
2022-12-25 14:30:25 +0100

    Create Footer Silhouette

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+// import react
+import React from "react";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 20vh;
+  background-color: #f3e1fa;
+`;
+const Footer = () => {
+  return <Container></Container>;
+};
+
+export default Footer;
