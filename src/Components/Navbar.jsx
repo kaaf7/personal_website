8a27b6dab947f3ae6480fd2859c35a828aced1fa
kaaf7 <amer.m.karim@gmail.com>
2022-12-27 22:59:48 +0100

    add parallax and edit dimensions

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,51 @@
-import React from "react";
+import { React, useEffect } from "react";
 import styled from "styled-components";
+import { gsap, ScrollTrigger } from "gsap/all";
 
 const Container = styled.div`
-  width: 100vw;
-  height: 10vh;
-  background-color: #f3e1fa;
+  height: 8vh;
+  // background: #50e3fd;
+  background-color: #f9f9f9;
+  text-align: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #000000;
   position: fixed;
+  width: 100%;
+  left: 0;
+  top: 0;
+  z-index: 100000;
+  transition: ease 0.4s;
+  &--scrolled {
+    height: 40px;
+  }
 `;
 const Navbar = () => {
-  return <Container></Container>;
+  gsap.registerPlugin(ScrollTrigger);
+
+  useEffect(() => {
+    let sectionAnimation = gsap.context(() => {
+      const showAnim = gsap
+        .from(".navbar", {
+          yPercent: -100,
+          paused: true,
+          duration: 0.4,
+        })
+        .progress(1);
+
+      ScrollTrigger.create({
+        start: "top top",
+        end: 99999,
+        onUpdate: (self) => {
+          self.direction === -1 ? showAnim.play() : showAnim.reverse();
+        },
+      });
+    });
+    return () => sectionAnimation.revert();
+  }, []);
+
+  return <Container className="navbar"></Container>;
 };
 
 export default Navbar;
