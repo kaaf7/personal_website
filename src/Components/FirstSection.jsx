76ec40faceee3c75dcff11a1554ca4f577a54f40
kaaf7 <amer.m.karim@gmail.com>
2022-12-27 22:59:36 +0100

    add parallax and edit dimensions

--- src/Components/FirstSection.jsx
+++ src/Components/FirstSection.jsx
@@ -1,46 +1,53 @@
 import { React, useEffect } from "react";
 import styled from "styled-components";
 import img from "../assets/Lower_Section.png";
-
 import { gsap, ScrollTrigger } from "gsap/all";
-
 const Container = styled.div`
-  height: 100vh;
+  height: 90vh;
   width: 100%;
   display: flex;
-  justify-content: center;
-  align-items: flex-start;
-  border-bottom: 1px solid black;
+  align-items: flex-end;
+  border-bottom: .5vh solid black;
+  background-color: #f6dfeb;
 `;
+
 const TextContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   height: 100%;
-  width: 20%;
+  width: 15%;
+  margin-right: 20vh;
   position: relative;
   overflow: hidden;
+  background-color: none;
 `;
 const Text = styled.h1`
   color: black;
   font-size: 20vh;
   position: relative;
+  background-color: none;
+  font-family: "Fredoka One", cursive, sans-serif;
 `;
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 100%;
   position: relative;
   display: flex;
   justify-content: center;
   align-items: flex-end;
-  flex: 1;
   overflow: hidden;
 `;
 
 const WelcomImage = styled.img`
   overflow: hidden;
-  transform: scale(1.2);
+  transform: scale(1);
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+  overflow: hidden;
+  overflow-x: hidden;
 `;
 
 const FirstSection = () => {
@@ -48,10 +55,24 @@ const FirstSection = () => {
   useEffect(() => {
     let sectionAnimation = gsap.context(() => {
       gsap.to(".Text", {
+        duration: 1.5,
+        y: -40,
+        repeat: -1,
+        yoyo: true,
+        scrollTrigger: {
+          trigger: ".firstSectionContainer",
+          start: "top bottom",
+          toggleActions: "play pause play pause",
+        },
+        ease: "sine.inOut",
+      });
+
+      gsap.to(".Text", {
         duration: 20,
         x: 100,
+        y:-400,
         scrollTrigger: {
-          trigger: ".Image",
+          trigger: ".firstSectionContainer",
           markers: false,
           start: "200px 200px",
           end: "bottom top",
@@ -62,7 +83,19 @@ const FirstSection = () => {
         duration: 20,
         x: -100,
         scrollTrigger: {
-          trigger: ".Image",
+          trigger: ".firstSectionContainer",
+          markers: false,
+          start: "200px 200px",
+          end: "bottom top",
+          scrub: true,
+        },
+      });
+
+      gsap.to(".backGround", {
+        duration: 20,
+        x: 200,
+        scrollTrigger: {
+          trigger: ".firstSectionContainer",
           markers: false,
           start: "200px 200px",
           end: "bottom top",
@@ -74,12 +107,12 @@ const FirstSection = () => {
   }, []);
 
   return (
-    <Container>
+    <Container className="firstSectionContainer">
       <Wrapper>
         <WelcomImage className="Image" src={img}></WelcomImage>
       </Wrapper>
-      <TextContainer>
-        <Text className="Text">HI</Text>
+      <TextContainer className="Text">
+        <Text>Hi</Text>
       </TextContainer>
     </Container>
   );
