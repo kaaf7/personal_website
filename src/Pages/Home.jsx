582ab339eb50d3774b38a86006a55ab3c0db62ea
kaaf7 <amer.m.karim@gmail.com>
2022-12-27 23:02:24 +0100

    add parallax and edit dimensions

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import { React } from "react";
 import Navbar from "../Components/Navbar";
-import UpperSection from "../Components/UpperSection";
-import MidSection from "../Components/MidSection";
-import LowerSection from "../Components/LowerSection";
+import FirstSection from "../Components/FirstSection";
+import SecondSection from "../Components/SecondSection";
+import ThirdSection from "../Components/ThirdSection";
+import ForthSection from "../Components/ForthSection";
+import FifthSection from "../Components/FifthSection";
 import Footer from "../Components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <UpperSection />
-      <MidSection />
-      <LowerSection />
-      <Footer />
+      <FirstSection />
+      <SecondSection />
+      <ThirdSection />
+      <ForthSection
+        container={"containerA"}
+        rightHand={"rightHandA"}
+        leftHand={"leftHandA"}
+        laptop={"laptopA"}
+        phone={"phoneA"}
+        text={"textA"}
+        color={"#f6dfeb"}
+      ></ForthSection>
+      <ForthSection
+        container={"containerB"}
+        rightHand={"rightHanB"}
+        leftHand={"leftHandB"}
+        laptop={"laptopB"}
+        phone={"phoneB"}
+        text={"textB"}
+        color={" #ffda2d"}
+      ></ForthSection>
+      <FifthSection />
     </div>
   );
 };
