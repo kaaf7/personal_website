843b9d9edafdc80336f5363974f5a9748967af0c
kaaf7 <amer.m.karim@gmail.com>
2022-12-28 18:40:30 +0100

    add my tech stack and chaneg font size

--- src/Components/ThirdSection.jsx
+++ src/Components/ThirdSection.jsx
@@ -30,7 +30,8 @@ const TextContainer = styled.div`
 `;
 const Text = styled.h1`
   color: black;
-  font-size: 8vh;
+  font-size: 6vh;
+  width: 100%;
   margin: 0;
   font-family: "Fredoka One", cursive, sans-serif;
 `;
@@ -90,8 +91,90 @@ const ThirdSection = () => {
         <WelcomImage src={img}></WelcomImage>
       </Wrapper>
       <TextContainer className="thirdSectionText">
-        <Text>IM A FRONTEND DEV</Text>
-        <Text>AND HERE IS MY WORK</Text>
+        <Text>I'M A FRONTEND DEVELOPER</Text>
+        <p align="left">
+          {" "}
+          <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
+              alt="react"
+              width="120"
+              height="120"
+            />{" "}
+          </a>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
+              alt="javascript"
+              width="120"
+              height="120"
+            />{" "}
+          </a>{" "}
+          <a
+            href="https://www.w3schools.com/css/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
+              alt="css3"
+              width="120"
+              height="120"
+            />{" "}
+          </a>{" "}
+          <a href="https://www.w3.org/html/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
+              alt="html5"
+              width="120"
+              height="120"
+            />{" "}
+          </a>
+          <a href="https://nodejs.org" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
+              alt="nodejs"
+              width="120"
+              height="120"
+            />{" "}
+          </a>
+          <a href="https://expressjs.com" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
+              alt="express"
+              width="120"
+              height="120"
+            />{" "}
+          </a>{" "}
+          <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
+              alt="mongodb"
+              width="120"
+              height="120"
+            />{" "}
+          </a>{" "}
+          <a href="https://redux.js.org" target="_blank" rel="noreferrer">
+            {" "}
+            <img
+              src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
+              alt="redux"
+              width="120"
+              height="120"
+            />{" "}
+          </a>
+        </p>{" "}
       </TextContainer>
     </Container>
   );
