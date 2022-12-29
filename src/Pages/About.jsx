fbbc99d7031ba2041e60d6766923afb540a87334
kaaf7 <amer.m.karim@gmail.com>
2022-12-29 22:06:58 +0100

    edit background color and edit margins

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,28 +7,42 @@ const Container = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
+  background-color: #efefef;
 `;
 const Wrapper = styled.div`
+  margin-top: 20vh;
   height: 100%;
   width: 100%;
-  margin-left: 5vw;
-  margin-right: 5vw;
   display: flex;
-  justify-content: center; ;
+  background-color: #efefef;
 `;
 const LeftSection = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   text-align: center;
+  margin-left: 5vw;
   height: 100%;
+  width: 50%;
+`;
+const BioTitle = styled.h1`
+  font-size: 5vh;
+  margin-bottom: 0;
+`;
+const BioText = styled.div`
+  font-size: 1vw;
+  margin-top: 3vh;
+`;
+const PersonalPhoto = styled.img`
+  background-color: #989898;
+  height: 60%;
   width: 50%;
 `;
 const RightSection = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   text-align: center;
   height: 100%;
@@ -39,8 +53,41 @@ const About = () => {
     <Container>
       <Navbar />
       <Wrapper>
-        <LeftSection></LeftSection>
-        <RightSection></RightSection>
+        <LeftSection>
+          <BioTitle>About Me</BioTitle>
+          <BioText>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+            aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+            mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum
+            semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          </BioText>{" "}
+          <BioText>
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem.
+            Nulla consequat massa quis enim. Donec pede justo, fringilla vel,
+            aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
+            imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+            mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum
+            semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
+            Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+          </BioText>
+        </LeftSection>
+        <RightSection>
+          {" "}
+          <PersonalPhoto />
+        </RightSection>
       </Wrapper>
     </Container>
   );
