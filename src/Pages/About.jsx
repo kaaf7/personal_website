5adbbbf3e9e0e9281ce18108514bad386bfe6965
kaaf7 <amer.m.karim@gmail.com>
2022-12-29 20:55:56 +0100

    create Wrapper

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,7 +3,7 @@ import Navbar from "../Components/Navbar";
 import styled from "styled-components";
 
 const Container = styled.div``
-
+const Wrapper = styled.div``
 
 const About = () => {
   return (
