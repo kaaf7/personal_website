423b273685254db409a16b517997c3ffc52e626c
kaaf7 <amer.m.karim@gmail.com>
2022-12-29 20:54:39 +0100

    import Navbar and create Container

---
+++ src/Pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from "../Components/Navbar";
+import styled from "styled-components";
+
+const Container = styled.div``
+
+
+const About = () => {
+  return (
+    <div><Navbar/></div>
+  )
+}
+
+export default About
