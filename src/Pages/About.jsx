60debd6fb8675bc923b799a829dbb758b572bd03
kaaf7 <amer.m.karim@gmail.com>
2022-12-29 21:11:21 +0100

    create right&left section, dimensions and styling

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,14 +1,49 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../Components/Navbar";
 import styled from "styled-components";
 
-const Container = styled.div``
-const Wrapper = styled.div``
-
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  margin-left: 5vw;
+  margin-right: 5vw;
+  display: flex;
+  justify-content: center; ;
+`;
+const LeftSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  width: 50%;
+`;
+const RightSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  height: 100%;
+  width: 50%;
+`;
 const About = () => {
   return (
-    <div><Navbar/></div>
-  )
-}
+    <Container>
+      <Navbar />
+      <Wrapper>
+        <LeftSection></LeftSection>
+        <RightSection></RightSection>
+      </Wrapper>
+    </Container>
+  );
+};
 
-export default About
+export default About;
