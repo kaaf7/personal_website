5edd614b72f4ff9a6693f64ffcc1ab55f7c60651
kaaf7 <amer.m.karim@gmail.com>
2022-12-30 16:03:21 +0100

    import Navbar and create form

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../Components/Navbar";
 import styled from "styled-components";
 const Container = styled.div`
@@ -14,8 +14,17 @@ const Wrapper = styled.div`
   width: 100%;
   display: flex;
   background-color: #efefef;
-`
-
+`;
+const TextContainer = styled.div`
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+const Title = styled.h1`
+  font-size: 5vh;
+`;
+const Text = styled.p``;
 const LeftSection = styled.div`
   display: flex;
   flex-direction: column;
@@ -25,6 +34,33 @@ const LeftSection = styled.div`
   margin-left: 5vw;
   height: 100%;
   width: 50%;
+  background-color: yellow;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+`;
+const Input = styled.input`
+  flex: 1;
+  min-width: 40%;
+  margin: 10px 0;
+  padding: 10px;
+  font-family: "Lexend", sans-serif;
+  &:focus {
+    outline: 0.1px solid lightgrey;
+  }
+`;
+const Button = styled.button`
+  width: 40%;
+  border: none;
+  padding: 2vh 5vh;
+  font-family: "Lexend", sans-serif;
+  background-color: #13b8ce;
+  color: white;
+  cursor: pointer;
+  margin-top: 1.5vh;
 `;
 const RightSection = styled.div`
   display: flex;
@@ -37,8 +73,33 @@ const RightSection = styled.div`
 `;
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
-}
+    <Container>
+      <Navbar />
+      <Wrapper>
+        <LeftSection>
+          <TextContainer>
+            <Title>Contact Me</Title>
+            <Text>hi</Text>
+          </TextContainer>
 
-export default Contact
+          <Form>
+            <Input placeholder="Name" />
+            <Input placeholder="Email" /> <Input placeholder="Subject" />{" "}
+            <textarea
+              type="text"
+              placeholder="Message"
+              style={{
+                height: "40vh",
+                resize: "none",
+              }}
+            />
+            <Button>SEND</Button>
+          </Form>
+        </LeftSection>
+        <RightSection></RightSection>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Contact;
