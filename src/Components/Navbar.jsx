0cf9ecfdc165865feeca838367fa806d0b1a7e03
kaaf7 <amer.m.karim@gmail.com>
2022-12-30 23:25:15 +0100

    add github link and anchor

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { React, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { gsap, ScrollTrigger } from "gsap/all";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 
 const Container = styled.div`
-  height: 10vh;
+  height: 8vh;
   background-color: #fbf9ff;
   color: #313131;
   position: fixed;
@@ -13,9 +14,9 @@ const Container = styled.div`
   left: 0;
   top: 0;
   z-index: 100000;
-  transition: ease 0.4s;
+  transition: ease 0.2s;
   &--scrolled {
-    height: 40px;
+    height: 4vh;
   }
 `;
 
@@ -30,23 +31,25 @@ const Wrapper = styled.div`
 `;
 
 const NavbarItem = styled.button`
-  margin-left: 2vh;
-  margin-right: 2vh;
+  margin-left: 1vh;
+  margin-right: 1vh;
   width: 10vh;
   height: 5vh;
   font-weight: 20;
   justify-content: center;
   align-items: center;
   text-align: center;
-  font-family: "Fredoka One", sans-serif;
+  font-family: "Signika", sans-serif;
+  font-size: 1.5vh;
   border-radius: 1vh;
+  font-weight: 400;
   border: 0.1px solid lightgrey;
   background: none;
-  transition: .5s;
+  transition: 0.5s;
   cursor: pointer;
   :hover {
     transform: scale(1.1);
-    transition: .5s;
+    transition: 0.5s;
   }
 `;
 const Logo = styled.h1`
@@ -54,7 +57,8 @@ const Logo = styled.h1`
   margin-right: 2vh;
   font-size: 2.5vw;
   color: #313131;
-  font-family: "Fredoka One", sans-serif;
+  font-family: "Signika", sans-serif;
+  cursor: pointer;
 `;
 
 const RightItems = styled.div`
@@ -69,6 +73,12 @@ const IconContainer = styled.div`
   align-items: center;
 `;
 const Navbar = () => {
+  const gitHubLink = "https://github.com/kaaf7"
+  const navigate = useNavigate();
+  const NavigateDir = (dir) => {
+    navigate(dir);
+  };
+
   gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
@@ -95,7 +105,13 @@ const Navbar = () => {
   return (
     <Container className="navbar">
       <Wrapper>
-        <Logo>KARIM.</Logo>
+        <Logo
+          onClick={() => {
+            NavigateDir("/");
+          }}
+        >
+          KARIM.
+        </Logo>
         <RightItems>
           <NavbarItem>
             <IconContainer>
@@ -103,13 +119,36 @@ const Navbar = () => {
             </IconContainer>
           </NavbarItem>
           <NavbarItem>
-            <IconContainer>
-              <GitHubIcon sx={{ marginRight: ".2vw" }}></GitHubIcon>GitHub
-            </IconContainer>
+            <a
+              style={{ color: "black", textDecoration: "none" }}
+              href={gitHubLink}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {" "}
+              <IconContainer>
+                <GitHubIcon
+                  sx={{ marginRight: ".2vw", color: "black" }}
+                ></GitHubIcon>
+                GitHub
+              </IconContainer>{" "}
+            </a>
           </NavbarItem>
           <NavbarItem>Projects</NavbarItem>
-          <NavbarItem>About</NavbarItem>
-          <NavbarItem>Contact</NavbarItem>
+          <NavbarItem
+            onClick={() => {
+              NavigateDir("/about");
+            }}
+          >
+            About
+          </NavbarItem>
+          <NavbarItem
+            onClick={() => {
+              NavigateDir("/contact");
+            }}
+          >
+            Contact
+          </NavbarItem>
         </RightItems>
       </Wrapper>
     </Container>
