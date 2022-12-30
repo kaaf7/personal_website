743fecfe05f0a0db11f611d51cb1a154e3f39cca
kaaf7 <amer.m.karim@gmail.com>
2022-12-30 15:08:17 +0100

    create Container and Wrapper

---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  background-color: #efefef;
+`;
+const Wrapper = styled.div`
+  margin-top: 20vh;
+  height: 100%;
+  width: 100%;
+  display: flex;
+  background-color: #efefef;
+`;
+const Contact = () => {
+  return (
+    <div>Contact</div>
+  )
+}
+
+export default Contact
