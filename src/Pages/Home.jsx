03e362cdc5f463256aaa2e912522063158cafab5
kaaf7 <amer.m.karim@gmail.com>
2023-01-03 15:21:35 +0100

    remove forth section

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,36 +3,19 @@ import Navbar from "../Components/Navbar";
 import FirstSection from "../Components/FirstSection";
 import SecondSection from "../Components/SecondSection";
 import ThirdSection from "../Components/ThirdSection";
-import ForthSection from "../Components/ForthSection";
 import FifthSection from "../Components/FifthSection";
+import img from "../assets/Lower_Section.png";
 import Footer from "../Components/Footer";
 const Home = () => {
   const ecommUrl = "https://ecomm-frontend-client.onrender.com/";
-
   return (
     <div>
       <Navbar />
-      <FirstSection />
+      <FirstSection fontSize="20vh" imgsrc={img} text ="Hi"/>
       <SecondSection />
       <ThirdSection />
-      <ForthSection
-        container={"containerA"}
-        rightHand={"rightHandA"}
-        leftHand={"leftHandA"}
-        laptop={"laptopA"}
-        phone={"phoneA"}
-        text={"textA"}
-        dir={ecommUrl}
-      ></ForthSection>
-      <ForthSection
-        container={"containerB"}
-        rightHand={"rightHanB"}
-        leftHand={"leftHandB"}
-        laptop={"laptopB"}
-        phone={"phoneB"}
-        text={"textB"}
-      ></ForthSection>
       <FifthSection />
+      <Footer />
     </div>
   );
 };
