7df3a8178ebb1c316287d7cd1afcfafdb0f1853f
kaaf7 <amer.m.karim@gmail.com>
2023-01-06 22:43:42 +0100

    add new description to project section

--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -4,14 +4,21 @@ import FirstSection from "../Components/FirstSection";
 import ForthSection from "../Components/ForthSection";
 import Footer from "../Components/Footer";
 import img from "../assets/check_projects.png";
+import ecommProject from "../assets/ecomm.png";
+import ecommProject_phone from "../assets/ecomm_phone.png";
+import menchProject from "../assets/mensch.png";
+import menchProject_phone from "../assets/mensch_phone.png";
 const ecommUrl = "https://ecomm-frontend-client.onrender.com/";
+const ecommDescription =
+  "Bankai is a high fashion brand, Bankai means the final form in Japanese, I have built both client and server, for the client side I used ReactJS, Redux toolkit, and Styled Component for CSS styling. I have also used libraries like Axios. On the other side, the server was built using NodeJS and ExpressJs  alongside  Mongoose  and  MongoDB as a database";
 
 const Projects = () => {
   return (
     <div>
       <Navbar />
-      <FirstSection fontSize ="10vh" imgsrc={img} text="Projects"></FirstSection>
+      <FirstSection fontSize="10vh" imgsrc={img} text="Projects"></FirstSection>
       <ForthSection
+        description={ecommDescription}
         container={"containerA"}
         rightHand={"rightHandA"}
         leftHand={"leftHandA"}
@@ -19,8 +26,10 @@ const Projects = () => {
         phone={"phoneA"}
         text={"textA"}
         dir={ecommUrl}
-        horizontalDirection ="900"
-        verticalDirection ="700"
+        horizontalDirection="900"
+        verticalDirection="700"
+        laptopImage={ecommProject}
+        phoneImage={ecommProject_phone}
       ></ForthSection>
       <ForthSection
         container={"containerB"}
@@ -29,10 +38,12 @@ const Projects = () => {
         laptop={"laptopB"}
         phone={"phoneB"}
         text={"textB"}
-        horizontalDirection ="-900"
-        verticalDirection ="700"
+        horizontalDirection="-900"
+        verticalDirection="700"
+        laptopImage={menchProject}
+        phoneImage={menchProject_phone}
       ></ForthSection>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
